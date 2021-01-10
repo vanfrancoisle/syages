22a8042918d10c2131ece92f0bd24e94a993a54d
NAF22 <73890085+NAF22@users.noreply.github.com>
2021-01-10 02:29:46 +0100

    Add files via upload

--- professeur/profAccueil.php
+++ professeur/profAccueil.php
@@ -4,6 +4,9 @@ require "../general/debut.php";
 echo '<link rel="stylesheet" type="text/css" href="../css/professeur/accueil_prof.css">';
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
 echo "<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>";
+echo '  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-circle-progress/1.2.2/circle-progress.min.js"></script>';
 require '../general/debut-2.php';
 $h3='Menu-Professeur';
 require "../general/navbanner-professeur.php";
@@ -11,10 +14,19 @@ require '../general/BDDsyages.php';
 $m = BDDsyages::getBddsyages(2);
 
 ?>
+
+<div class="body" id="body">
+<div class="melbanner">
+    <button id="btn-menu1" onclick="show_hide()"><img src="../img/menu.png" id="menu"></button>
+    <img src="../img/logo.png" id="logo"/>
+    <input type="text" placeholder="Entrez des mots-clés" id="searchbar"><input type="submit" value="Rechercher" id="submitbutton">
+    <img src="../img/david.jpg" id="user"/>
+</div>
 <?php 
   	$mat= $m-> pourcentage_UneMatiere("Mathématiques");
   	$ph= $m-> pourcentage_UneMatiere("Physique");
-  	$to= $m-> pourcentage();
+	  $to= $m-> pourcentage();
+	 
 	
 
   	$i=0;
@@ -45,7 +57,7 @@ $m = BDDsyages::getBddsyages(2);
   	if (count($to)==0)
   		$total= 0;
   	else
-  		$total= $k/count($to);
+		  $total= $k/count($to); 
   	
 
 
@@ -56,7 +68,7 @@ $m = BDDsyages::getBddsyages(2);
     <h2 class="heading"> PROMO ACTIVE </h2>
     <div class="wrapper2">
       
-      <div class="card">
+      <div class="card 1">
         <div class="circle">
           <div class="bar"></div>
           <div class="box"><span></span></div>
@@ -96,10 +108,15 @@ $m = BDDsyages::getBddsyages(2);
       $(".circle .bar").circleProgress(options).on('circle-animation-progress',
       function(event, progress, stepValue){
         $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+	  });
+	  
+	  $(".1 .bar").circleProgress({
+        value: <?php echo $maths; ?>,
       });
       $(".2 .bar").circleProgress({
         value: <?php echo $phy; ?>,
-      });
+	  });
+	  
       $(".3 .bar").circleProgress({
         value: <?php echo $total; ?>,
       });
@@ -147,29 +164,16 @@ $m = BDDsyages::getBddsyages(2);
 		<div class="tabpromo">
 			<div class="scroll">
 			<table class="tabB">
-			<tr><th>Date début</th><th>Date fin</th><th>Nom de la promotion</th><th>Inscrits</th><th>Moyenne générale</th></tr>
+			<tr><th>Date début</th><th>Date fin</th><th>DAEUOptions</th><th>Nom de la promotion</th><th>Matières</th></tr>
 							<?php 
-                                    $tabPromo= $m->lespromoAnciennes();
-                                    if (!empty($tabPromo)){
-                                        $anneefin = explode("-",$tabPromo[0]["datedebut"]);
-                                        $anneedebut = explode("-",$tabPromo[0]["dateFin"]);
-                                        echo '<tr class="titre_annee"><td colspan="7">Année '.$anneedebut[0].'-'.$anneefin[0].'</td></tr>';
-                                        foreach ($tabPromo as $key => $value) {
-                                            $fin = explode("-",$tabPromo[0]["datedebut"]);
-                                            $debut = explode("-",$tabPromo[0]["dateFin"]);
-                                            if ($anneedebut[0]!=$debut[0] or $anneefin[0]!=$fin[0]){
-                                                echo '<tr class="titre_annee"><td colspan="7">Année '.$anneedebut[0].'-'.$anneefin[0].'</td></tr>';
-                                            }
-											echo '<tr><td>'.$value["datedebut"].'</td><td>'.$value["dateFin"].'</td><td><a href="#">'.$value["nomPromo"].'</a></td><td>'.$bd->get_nbInscrit($value["idPromotion"])[0].'</td></td><td>'.$bd->get_moyennePromo($value["idPromotion"])[0].'</td></tr>';
-                                        }
-                                    } else {
-                                    echo '<tr class="titre_annee"><td colspan="7">Aucune promotion ancienne</td></tr>';
-                                    }
-                            ?>
+									$oldProm= $m->lespromoAnciennes();
+									foreach ($oldProm as $new){
+										echo '<tr><td>' . $new['DateDebut'] . '</td><td>' . $new['DateFin'] . '</td><td>' . $new['Option'] . '</td><td>' . $new['NomPromo'] . '</td><td>' . $new['matieres'] . '</td></tr>';
+									}?>
 			</table>
 			</div>
 		</div>
 		<!------------------------------------------------------------------------------------>
 <?php
-require "../general/fin.html";
+require "../general/fin.php";
 ?>
